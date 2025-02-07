ce77bcb8733d7f6c16b39e6effa17dc7c45d9fda
Juan Pedro de León <70114884+juanpdeleon@users.noreply.github.com>
2025-02-07 03:01:00 -0300

    Add files via upload

---
+++ Febrero_2025/libreria.py
@@ -0,0 +1,191 @@
+import cv2
+import os
+import shutil
+import json
+import re
+from ultralytics import solutions
+
+# Diccionario de clases
+CLASES = {
+    0: "MOTO",
+    1: "OMNIBUS",
+    3: "AUTO",
+    4: "Camion Liviano",
+    5: "Camion Pesado"
+}
+
+def preocess_input_video(input_dir, output_dir, backup_dir, Log_dir):
+    """Procesa los videos en el directorio de entrada y genera salidas."""
+    cropped_box = os.path.join(output_dir, "cropped")
+    saved_json = os.path.join(output_dir, "jsonfile")
+    camiones_dir = os.path.join(output_dir, "camiones")
+
+    if not os.path.exists(input_dir):
+        print("El directorio original no existe.")
+        return
+
+    # Crear directorios si no existen
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(cropped_box, exist_ok=True)
+    os.makedirs(camiones_dir, exist_ok=True)
+    os.makedirs(saved_json, exist_ok=True)
+    os.makedirs(backup_dir, exist_ok=True)
+    os.makedirs(Log_dir, exist_ok=True)
+
+    # Obtener archivos de video
+    content = [f for f in os.listdir(input_dir) if f.endswith('.mp4')]
+
+    if not content:
+        print("Directorio de videos está vacío.")
+        return
+
+    for archive in content:
+        print(f"Procesando: {archive}")
+        path_video_file = os.path.join(input_dir, archive)
+
+        status = count_specific_classes(
+            path_video_file, "output_specific_classes.avi", "caminosRurales.pt",
+            [0,1, 2, 3,4,5], cropped_box, saved_json, archive
+        )
+
+        if status:
+            shutil.copy(path_video_file, os.path.join(backup_dir, archive))
+            print(f"Video copiado: {path_video_file} -> {backup_dir}/{archive}")
+        else:
+            print("Error procesando el archivo de video.")
+
+
+def count_specific_classes(video_path, output_video_path, model_path, classes_to_count, cropped_box, saved_json, archive):
+    """Cuenta clases específicas de objetos en un video."""
+    cap = cv2.VideoCapture(video_path)
+    assert cap.isOpened(), "Error al abrir el archivo de video"
+
+    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+    video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+    # Líneas de detección
+    line1_y = 250
+    line2_y = 300
+    line_points_max = [(10, line1_y), (1300, line1_y)]  # Línea principal de detección
+
+    counter = solutions.ObjectCounter(
+        show=True, region=line_points_max, model=model_path,
+        classes=classes_to_count, verbose=False, line_thickness=1
+    )
+
+    frame_count = 0
+    objects_crossed = set()
+    previous_positions = {}
+
+    while cap.isOpened():
+        success, im0 = cap.read()
+        if not success:
+            print("Video completado o sin frames válidos.")
+            break
+
+        # Procesar el frame con YOLOv11
+        results = counter.model(im0)  # Obtener resultados de YOLOv11
+        im0 = counter.count(im0)  # Dibujar contadores y bounding boxes
+
+        video_writer.write(im0)
+        cv2.line(im0, (10, line1_y), (1300, line1_y), (255, 100, 255), 1)  # Dibujar línea de detección
+
+        frame_count += 1
+
+        # Obtener class_ids, boxes y track_ids desde los resultados de YOLOv11
+        for result in results:
+            boxes = result.boxes.xyxy.cpu().numpy()  # Coordenadas de los bounding boxes
+            class_ids = result.boxes.cls.cpu().numpy().astype(int)  # IDs de las clases
+            track_ids = result.boxes.id.cpu().numpy().astype(int) if result.boxes.id is not None else []  # IDs de seguimiento
+
+            for track_id, box, class_id in zip(track_ids, boxes, class_ids):
+                x1, y1, x2, y2 = box
+                centroid = ((x1 + x2) // 2, (y1 + y2) // 2)
+                tipo_vehiculo = str(class_id) # CLASES.get(class_id, "unknown")
+
+                if line1_y < centroid[1] < line2_y:  # Rango de detección ajustado
+                    if track_id in previous_positions:
+                        previous_y = previous_positions[track_id][1]
+                        current_y = centroid[1]
+
+                        if current_y < previous_y:
+                            direccion = "IN"
+                        else:
+                            direccion = "OUT"
+                    else:
+                        direccion = "UNKNOWN"
+
+                    if track_id not in objects_crossed:
+                        print(f"Objeto {track_id} cruzó la línea en dirección: {direccion}")
+                        objects_crossed.add(track_id)
+
+                        nombre_captura = save_cropped_box(im0, box, track_id, frame_count, 100, cropped_box, archive)
+                        tiempo = formatear_nombre_archivo(nombre_captura)
+                        list_data = [direccion, nombre_captura + ".jpg", tipo_vehiculo, 0, 'XXX0000', tiempo]
+
+                        save_json_file(list_data, saved_json, nombre_captura)
+
+                previous_positions[track_id] = centroid
+
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            print("Reproducción detenida por el usuario.")
+            break
+
+    cap.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+    return True
+
+
+def save_cropped_box(im0, box, track_id, frame_count, incremento_porcentaje, output_dir, archive):
+    """Guarda una imagen recortada del bounding box."""
+    x1, y1, x2, y2 = map(int, box)
+    ancho_original = x2 - x1
+    alto_original = y2 - y1
+
+    factor_incremento = incremento_porcentaje / 100.0
+    incremento_ancho = ancho_original * factor_incremento
+    incremento_alto = alto_original * factor_incremento
+
+    centro_x = (x1 + x2) / 2
+    centro_y = (y1 + y2) / 2
+
+    nuevo_x1 = max(0, int(centro_x - (ancho_original / 2 + incremento_ancho / 2)))
+    nuevo_y1 = max(0, int(centro_y - (alto_original / 2 + incremento_alto / 2)))
+    nuevo_x2 = min(im0.shape[1], int(centro_x + (ancho_original / 2 + incremento_ancho / 2)))
+    nuevo_y2 = min(im0.shape[0], int(centro_y + (alto_original / 2 + incremento_alto / 2)))
+
+    cropped_box = im0[nuevo_y1:nuevo_y2, nuevo_x1:nuevo_x2]
+    output_path = os.path.join(output_dir, f"{archive}_track_{track_id}_frame_{frame_count}.jpg")
+    cv2.imwrite(output_path, cropped_box)
+    return f"{archive}_track_{track_id}_frame_{frame_count}"
+
+
+def formatear_nombre_archivo(nombre_archivo: str) -> str:
+    """Formatea el nombre del archivo para extraer fecha y hora."""
+    match = re.search(r'(\d{2}-\d{2}-\d{2})_(\d{2})-(\d{2})-(\d{2})-(\d{2})', nombre_archivo)
+    if match:
+        fecha = match.group(1)
+        hora = f"{match.group(2)}:{match.group(3)}:{match.group(4)}.{match.group(5)}"
+        return f"{fecha} {hora}"
+    else:
+        return "Formato inválido"
+
+
+def save_json_file(list_data, saved_json, file_name):
+    """Guarda los datos en un archivo JSON."""
+    file_name_path = os.path.join(saved_json, f"{file_name}.json")
+    datos_json = {
+        "direccion": list_data[0],
+        "ruta": list_data[1],
+        "tipo": list_data[2],
+        "ejes": list_data[3],
+        "matricula": list_data[4],
+        "time": list_data[5]
+    }
+    with open(file_name_path, "w") as archivo:
+        json.dump(datos_json, archivo, indent=4)
+
+    print(f"Archivo JSON creado: {file_name_path}")
+    return True
